be2e747169ca1269a4903598888aac66db2f8cfe
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-08-09 16:05:37 +0530

    2 files modified, 1 added, 1 deleted

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <svelte:head>
-    <script>
-    var canvas = document.createElement('canvas');
-var width = (canvas.width = window.innerWidth * 0.75);
-var height = (canvas.height = window.innerHeight * 0.75);
+  <script>
+  var canvas = document.createElement("canvas");
+var width = canvas.width = window.innerWidth * 0.75;
+var height = canvas.height = window.innerHeight * 0.75;
 document.body.appendChild(canvas);
 var gl = canvas.getContext('webgl');
 
-var mouse = { x: 0, y: 0 };
+var mouse = {x: 0, y: 0};
 
 var numMetaballs = 30;
 var metaballs = [];
@@ -18,7 +18,7 @@ for (var i = 0; i < numMetaballs; i++) {
     y: Math.random() * (height - 2 * radius) + radius,
     vx: (Math.random() - 0.5) * 3,
     vy: (Math.random() - 0.5) * 3,
-    r: radius * 0.75,
+    r: radius * 0.75
   });
 }
 
@@ -32,29 +32,20 @@ gl_Position = vec4(position, 0.0, 1.0);
 }
 `;
 
-var fragmentShaderSrc =
-  `
+var fragmentShaderSrc = `
 precision highp float;
 
-const float WIDTH = ` +
-  (width >> 0) +
-  `.0;
-const float HEIGHT = ` +
-  (height >> 0) +
-  `.0;
+const float WIDTH = ` + (width >> 0) + `.0;
+const float HEIGHT = ` + (height >> 0) + `.0;
 
-uniform vec3 metaballs[` +
-  numMetaballs +
-  `];
+uniform vec3 metaballs[` + numMetaballs + `];
 
 void main(){
 float x = gl_FragCoord.x;
 float y = gl_FragCoord.y;
 
 float sum = 0.0;
-for (int i = 0; i < ` +
-  numMetaballs +
-  `; i++) {
+for (int i = 0; i < ` + numMetaballs + `; i++) {
 vec3 metaball = metaballs[i];
 float dx = metaball.x - x;
 float dy = metaball.y - y;
@@ -83,14 +74,10 @@ gl.linkProgram(program);
 gl.useProgram(program);
 
 var vertexData = new Float32Array([
-  -1.0,
-  1.0, // top left
-  -1.0,
-  -1.0, // bottom left
-  1.0,
-  1.0, // top right
-  1.0,
-  -1.0, // bottom right
+  -1.0,  1.0, // top left
+  -1.0, -1.0, // bottom left
+  1.0,  1.0, // top right
+  1.0, -1.0, // bottom right
 ]);
 var vertexDataBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
@@ -98,14 +85,13 @@ gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
 
 var positionHandle = getAttribLocation(program, 'position');
 gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(
-  positionHandle,
-  2, // position is a vec2
-  gl.FLOAT, // each component is a float
-  gl.FALSE, // don't normalize values
-  2 * 4, // two 4 byte float components per vertex
-  0 // offset into each span of vertex data
-);
+gl.vertexAttribPointer(positionHandle,
+                       2, // position is a vec2
+                       gl.FLOAT, // each component is a float
+                       gl.FALSE, // don't normalize values
+                       2 * 4, // two 4 byte float components per vertex
+                       0 // offset into each span of vertex data
+                      );
 
 var metaballsHandle = getUniformLocation(program, 'metaballs');
 
@@ -116,10 +102,8 @@ function loop() {
     metaball.x += metaball.vx;
     metaball.y += metaball.vy;
 
-    if (metaball.x < metaball.r || metaball.x > width - metaball.r)
-      metaball.vx *= -1;
-    if (metaball.y < metaball.r || metaball.y > height - metaball.r)
-      metaball.vy *= -1;
+    if (metaball.x < metaball.r || metaball.x > width - metaball.r) metaball.vx *= -1;
+    if (metaball.y < metaball.r || metaball.y > height - metaball.r) metaball.vy *= -1;
   }
 
   var dataToSendToGPU = new Float32Array(3 * numMetaballs);
@@ -131,7 +115,7 @@ function loop() {
     dataToSendToGPU[baseIndex + 2] = mb.r;
   }
   gl.uniform3fv(metaballsHandle, dataToSendToGPU);
-
+  
   //Draw
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
@@ -144,7 +128,7 @@ function compileShader(shaderSource, shaderType) {
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw 'Shader compile failed with: ' + gl.getShaderInfoLog(shader);
+    throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
   }
 
   return shader;
@@ -166,278 +150,50 @@ function getAttribLocation(program, name) {
   return attributeLocation;
 }
 
-canvas.onmousemove = function (e) {
+canvas.onmousemove = function(e) {
   mouse.x = e.clientX;
   mouse.y = e.clientY;
-};
-
-    </script>
+}
+  </script>
 </svelte:head>
 
-<script lang="ts">
-  import Counter from './lib/Counter.svelte'
-  import EmailSub from './lib/EmailSub.svelte'
-</script>
+<div class="container-fluid">
+  <main>
+  <div class="container">
+    <div class="box">
+      <span />
+      <div class="content">
+        <h2>Developer</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <a href="#">Read More</a>
+      </div>
+    </div>
 
-<main>
- <div class="container">
-  <div class="box">
-    <span></span>
-    <div class="content">
-      <h2>Developer</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="#">Read More</a>
+    <div class="box">
+      <span />
+      <div class="content">
+        <h2>Musician</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <a href="#">Read More</a>
+      </div>
+    </div>
+    <div class="box">
+      <span />
+      <div class="content">
+        <h2>Card Three</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <a href="#">Read More</a>
+      </div>
     </div>
   </div>
-
-  <div class="box">
-    <span></span>
-    <div class="content">
-      <h2>Card two</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="#">Read More</a>
-    </div>
-  </div>
-  <div class="box">
-    <span></span>
-    <div class="content">
-      <h2>Card Three</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="#">Read More</a>
-    </div>
-  </div>
-</div>
 </main>
-
-<style>
-*
-{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: consolas;
-}
-
-body
-{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background: #1d061a;
-  font-family: 'Alatsi', sans-serif;
-  margin: 0;
-  overflow: hidden;
-  background: black;
-}
-
-.container
-{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  padding: 40px 0;
-}
-
-.container .box
-{
-  position: relative;
-  width: 320px;
-  height: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 40px 30px;
-  transition: 0.5s;
-}
-
-.container .box::before
-{
-  content:' ';
-  position: absolute;
-  top: 0;
-  left: 50px;
-  width: 50%;
-  height: 100%;
-  text-decoration: none;
-  background: #fff;
-  border-radius: 8px;
-  transform: skewX(15deg);
-  transition: 0.5s;
-}
-
-.container .box::after
-{
-  content:'';
-  position: absolute;
-  top: 0;
-  left: 50;
-  width: 50%;
-  height: 100%;
-  background: #fff;
-  border-radius: 8px;
-  transform: skewX(15deg);
-  transition: 0.5s;
-  filter: blur(30px);
-}
-
-.container .box:hover:before,
-.container .box:hover:after
-{
-  transform: skewX(0deg);
-  left: 20px;
-  width: calc(100% - 90px);
-  
-}
-
-.container .box:nth-child(1):before,
-.container .box:nth-child(1):after
-{
-  background: linear-gradient(315deg, #ffbc00, #ff0058)
-}
-
-.container .box:nth-child(2):before,
-.container .box:nth-child(2):after
-{
-  background: linear-gradient(315deg, #03a9f4, #ff0058)
-}
-
-.container .box:nth-child(3):before,
-.container .box:nth-child(3):after
-{
-  background: linear-gradient(315deg, #4dff03, #00d0ff)
-}
-
-.container .box span
-{
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 5;
-  pointer-events: none;
-}
-
-.container .box span::before
-{
-  content:'';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-  border-radius: 8px;
-  background: rgba(255, 255, 255, 0.1);
-  backdrop-filter: blur(10px);
-  opacity: 0;
-  transition: 0.1s;  
-  animation: animate 2s ease-in-out infinite;
-  box-shadow: 0 5px 15px rgba(0,0,0,0.08)
-}
-
-.container .box:hover span::before
-{
-  top: -50px;
-  left: 50px;
-  width: 100px;
-  height: 100px;
-  opacity: 1;
-}
-
-.container .box span::after
-{
-  content:'';
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 8px;
-  background: rgba(255, 255, 255, 0.1);
-  backdrop-filter: blur(10px);
-  opacity: 0;
-  transition: 0.5s;
-  animation: animate 2s ease-in-out infinite;
-  box-shadow: 0 5px 15px rgba(0,0,0,0.08);
-  animation-delay: -1s;
-}
-
-.container .box:hover span:after
-{
-  bottom: -50px;
-  right: 50px;
-  width: 100px;
-  height: 100px;
-  opacity: 1;
-}
-
-@keyframes animate
-{
-  0%, 100%
-  {
-    transform: translateY(10px);
-  }
-  
-  50%
-  {
-    transform: translate(-10px);
-  }
-}
-
-.container .box .content
-{
-  position: relative;
-  left: 0;
-  padding: 20px 40px;
-  background: rgba(255, 255, 255, 0.05);
-  backdrop-filter: blur(10px);
-  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-  z-index: 1;
-  transform: 0.5s;
-  color: #fff;
-}
-
-.container .box:hover .content
-{
-  left: -25px;
-  padding: 60px 40px;
-}
-
-.container .box .content h2
-{
-  font-size: 2em;
-  /* color: #fff; */
-  margin-bottom: 10px;
-}
-
-.container .box .content p
-{
-  font-size: 1.1em;
-  margin-bottom: 10px;
-  line-height: 1.4em;
-}
-
-.container .box .content a
-{
-  display: inline-block;
-  font-size: 1.1em;
-  color: #111;
-  background: #fff;
-  padding: 10px;
-  border-radius: 4px;
-  text-decoration: none;
-  font-weight: 700;
-  margin-top: 5px;
-}
-
-.container .box .content a:hover
-{
-  background: #ffcf4d;
-  border: 1px solid rgba(255, 0, 88, 0.4);
-  box-shadow: 0 1px 15px rgba(1, 1, 1, 0.2);
-}
-</style>
+</div>
