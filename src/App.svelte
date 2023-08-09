952d6e58036358fc8d1c6dd8d56d1d34eabc45d6
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-08-09 11:53:07 +0530

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,17 @@
 </script>
 
 <main>
-  <div>
   <div class="card">
   <p>Click to Count</p>
     <Counter />
   </div>
       <EmailSub />
-
 </main>
 
 <style>
+main{
+  border: 1px solid blue;
+  padding: 2rem;
+  border-radius: 2rem;
+}
 </style>
