2e54515987ff98549a69ab010d56e6336ebbaeac
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-08-09 11:51:28 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import EmailSub from './lib/EmailSub.svelte'
+</script>
+
+<main>
+  <div>
+  <div class="card">
+  <p>Click to Count</p>
+    <Counter />
+  </div>
+      <EmailSub />
+
+</main>
+
+<style>
+</style>
---
+++ src/lib/EmailSub.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+let yes = false
+const handleOnClick = () => {
+  alert("We will Start Spamming You!")
+}
+</script>
+
+<input bind:checked={yes} type="checkbox" /> Click to send spam email!
+
+{#if yes}
+<p>Yay!, We Will start to spam you soon!</p>
+{:else}
+<p>Click to get Spammed!</p>
+{/if}
+
+<button disabled={!yes} on:click={handleOnClick}>Spam Me</button>
